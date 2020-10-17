644f3469fcf0242735018c454ed3cd6893899a27
Max Fischer <max.v.fischer@gmail.com>
2020-10-17 21:30:56 +0200

    Add docstrings to ScaraRobot class

--- code/test_plot.py
+++ code/test_plot.py
@@ -7,7 +7,6 @@ if __name__ == "__main__":
         length_first_arm=SCARA_ROBOT_V1['length_first_arm'],
         length_second_arm=SCARA_ROBOT_V1['length_second_arm'],
         angle_per_motor_step=SCARA_ROBOT_V1['angle_per_motor_step'],
-        timeout_between_steps_sec=SCARA_ROBOT_V1['timeout_between_steps_sec'],
         art_config=FLOWER_2,
         visualization_mode=True,
         number_of_steps=1000
--- code/utils/scara.py
+++ code/utils/scara.py
@@ -1,17 +1,43 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from collections import deque
-from typing import Callable
+from typing import Callable, List, Tuple
 from matplotlib.animation import FuncAnimation
 
 
 class ScaraRobot:
+    """
+    SCARA robot using two arms.
+
+    See the paper "Formal Kinematic Analysis of the Two-Link Planar Manipulator"
+    (https://www.researchgate.net/publication/283377443_Formal_Kinematic_Analysis_of_the_Two-Link_Planar_Manipulator)
+    for inverse kinematic equations used.
+
+    Arguments
+    ---------
+    length_first_arm : int
+        Length of first arm.
+
+    length_second_arm : int
+        Length of second arm.
+
+    angle_per_motor_step : float
+        Number of degrees per step by the stepper motor.
+
+    art_config : dict
+        Config of the polar equation graph that is going to be drawn by the SCARA robot.
+
+    number_of_steps : int
+        Number of steps to complete one full 'circle' of the polar equation graph.
+
+    visualization_mode : bool
+        Visualization mode.
+    """
     def __init__(self,
                  length_first_arm: int,
                  length_second_arm: int,
                  angle_per_motor_step: float,
                  art_config: dict,
-                 timeout_between_steps_sec: float = 1,
                  number_of_steps: int = 1000,
                  visualization_mode: bool = False):
         self.length_first_arm = length_first_arm
@@ -25,7 +51,6 @@ class ScaraRobot:
             scale=art_config['scale'],
             num_steps=number_of_steps
         )
-        self.timeout_between_steps_sec = timeout_between_steps_sec
 
         self.current_route_index = 0
         self.current_actual_angle_first_arm = 0.
@@ -56,7 +81,7 @@ class ScaraRobot:
                           num_steps: int,
                           scale: float = 1,
                           lower_limit: float = 0,
-                          upper_limit: float = 2*np.pi):
+                          upper_limit: float = 2*np.pi) -> List[Tuple[float, float]]:
         """
         Generates a list of (x, y) coordinates, generated from a polar equation implemented as a lambda function.
 
@@ -69,18 +94,21 @@ class ScaraRobot:
             Example: lambda_function = eval("lambda theta: 3 + np.sin(10*theta)")
 
         num_steps : int
-            Number of samples to generate between lower_limit and upper_limit
+            Number of samples to generate between lower_limit and upper_limit.
+
+        scale : float
+            Scaling factor. Increases size of polar equation graph.
 
         lower_limit : float
-            Starting value in the list of theta
+            Starting value in the list of theta.
 
         upper_limit : float
-            Ending value in the list of theta
+            Ending value in the list of theta.
 
         Return
         ------
-            list
-                List of tuples, containing (x, y) coordinates
+        list
+            List of tuples, containing (x, y) coordinates.
         """
         import inspect
         lambda_args = inspect.getfullargspec(lambda_function).args
@@ -95,12 +123,31 @@ class ScaraRobot:
         return coordinates
 
     @staticmethod
-    def _difference_two_angles(angle_1, angle_2):
+    def _difference_two_angles(angle_1: float, angle_2: float) -> float:
+        """
+        Computes the absolute difference between two angles.
+
+        Parameters
+        ----------
+        angle_1 : float
+            First angle.
+
+        angle_2 : float
+            Second angle.
+
+        Return
+        ------
+        float
+            Absolute difference between angle_1 and angle_2.
+        """
         diff = np.abs(angle_1 - angle_2)
         diff = 360 - diff if diff > 180 else diff
         return diff
 
     def _update_current_numeric_angles(self):
+        """
+        Updates the current numeric angles of the first and second arms.
+        """
         positive_angle_first_arm, negative_angle_first_arm = self._current_numeric_angles_first_arm()
         positive_angle_second_arm, negative_angle_second_arm = self._current_numeric_angles_second_arm()
 
@@ -114,19 +161,20 @@ class ScaraRobot:
             self.current_actual_angle_first_arm = negative_angle_first_arm
             self.current_actual_angle_second_arm = negative_angle_second_arm
 
-    def _current_numeric_angles_first_arm(self, degrees=True):
+    def _current_numeric_angles_first_arm(self, degrees=True) -> Tuple[float, float]:
         """
-        Computes the current numeric angle of the first arm, using inverse kinematics.
+        Computes the two different numeric angles ('Elbow up' and 'Elbow down') of the first arm, using trigonometric
+        inverse kinematics.
 
         Parameters
         ----------
         degrees : bool
-            If the angle shall be returned as radians or degrees
+            If the angle shall be returned as radians or degrees.
 
         Return
         ------
         float
-            Current numeric angle of the first arm
+            Current numeric angle of the first arm.
         """
         x = self.arm_route_x_y_positions[self.current_route_index][0]
         y = self.arm_route_x_y_positions[self.current_route_index][1]
@@ -156,19 +204,20 @@ class ScaraRobot:
 
         return positive_angle, negative_angle
 
-    def _current_numeric_angles_second_arm(self, degrees=True):
+    def _current_numeric_angles_second_arm(self, degrees=True) -> Tuple[float, float]:
         """
-        Computes the current numeric angle of the second arm, using inverse kinematics.
+        Computes the two different numeric angles ('Elbow up' and 'Elbow down') of the second arm, using trigonometric
+        inverse kinematics.
 
         Parameters
         ----------
         degrees : bool
-            If the angle shall be returned as radians or degrees
+            If the angle shall be returned as radians or degrees.
 
         Return
         ------
         float
-            Current numeric angle of the second arm
+            Current numeric angle of the second arm.
         """
         x = self.arm_route_x_y_positions[self.current_route_index][0]
         y = self.arm_route_x_y_positions[self.current_route_index][1]
@@ -193,7 +242,16 @@ class ScaraRobot:
 
         return angle_positive, angle_negative
 
-    def _num_step_first_motor(self):
+    def _num_step_first_motor(self) -> Tuple[int, int]:
+        """
+        Computes the direction and number of steps of first stepper motor to end up as close as possible to the
+        current angle of the first arm.
+
+        Return
+        ------
+        Tuple[int, int]
+            Direction of arm ({-1 or 1}) and the number of steps to be taken.
+        """
         diff_numeric_actual_first_arm = self.current_actual_angle_first_arm - self.previous_actual_angle_first_arm
         direction = 1 if diff_numeric_actual_first_arm >= 0 else -1
         num_steps = np.abs(diff_numeric_actual_first_arm//self.angle_per_motor_step)
@@ -201,13 +259,31 @@ class ScaraRobot:
         return direction, num_steps
 
     def _num_step_second_motor(self):
+        """
+        Computes the direction and number of steps of second stepper motor to end up as close as possible to the
+        current angle of the second arm.
+
+        Return
+        ------
+        Tuple[int, int]
+            Direction of arm ({-1 or 1}) and the number of steps to be taken.
+        """
         diff_numeric_actual_second_arm = self.current_actual_angle_second_arm - self.previous_actual_angle_second_arm
         direction = 1 if diff_numeric_actual_second_arm >= 0 else -1
         num_steps = np.abs(diff_numeric_actual_second_arm // self.angle_per_motor_step)
 
         return direction, num_steps
 
-    def _compute_x_y_position_arms(self):
+    def _compute_x_y_position_arms(self) -> Tuple[dict, dict]:
+        """
+        Compute the (x, y) coordinates of the start and end point of each arm.
+
+        Return
+        ------
+        Tuple[dict, dict]
+            First dict contains the (x, y) coordinates of the first arm. The second dict contains the (x, y)
+            coordinates of the second arm.
+        """
         x_y_first_arm = {
             'x': [0, self.length_first_arm * np.cos(self.previous_actual_angle_first_arm*(np.pi/180))],
             'y': [0, self.length_first_arm * np.sin(self.previous_actual_angle_first_arm*(np.pi/180))]
@@ -227,7 +303,15 @@ class ScaraRobot:
 
         return x_y_first_arm, x_y_second_arm
 
-    def _init_function_visualization(self):
+    def _init_function_visualization(self) -> Tuple[plt.Line2D, plt.Line2D, plt.Line2D]:
+        """
+        Initial function to start matplotlib's FuncAnimation.
+
+        Return
+        ------
+        Tuple[plt.Line2D, plt.Line2D, plt.Line2D]
+            Tuple containing the plots of first arm, second arm and trajectory.
+        """
         x_y_first_arm, x_y_second_arm = self._compute_x_y_position_arms()
 
         self.first_arm_plot.set_data(x_y_first_arm['x'], x_y_first_arm['y'])
@@ -237,10 +321,19 @@ class ScaraRobot:
         return self.first_arm_plot, self.second_arm_plot, self.x_y_trajectory_plot
 
     def _visualization_step(self, i):
-        self._step()
+        """
+        Step function in matplotlib's FuncAnimation.
 
-        x = self.arm_route_x_y_positions[self.current_route_index][0]
-        y = self.arm_route_x_y_positions[self.current_route_index][1]
+        Parameters
+        ----------
+        i : int
+            Animation counter. Not used.
+
+        Return
+        ------
+        None
+        """
+        self._step()
 
         x_y_first_arm, x_y_second_arm = self._compute_x_y_position_arms()
         self.x_trajectory.append(x_y_second_arm['x'][1])
@@ -252,6 +345,10 @@ class ScaraRobot:
         self.x_y_trajectory_plot.set_data(list(self.x_trajectory), list(self.y_trajectory))
 
     def _step(self):
+        """
+        Single step. Iterates to next (x, y) coordinate, computes new angles of arms and computes direction and number
+        of steps for each arm.
+        """
         self._update_current_numeric_angles()
 
         dir_first_arm, steps_first_arm = self._num_step_first_motor()
