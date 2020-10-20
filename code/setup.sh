85ac1245bb67ad0751b03a299e2726ddf963b454
Max Fischer <max.v.fischer@gmail.com>
2020-10-20 20:54:39 +0200

    Add integration with firebase

---
+++ code/setup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+echo "Set GCloud credential environment variable"
+export GOOGLE_APPLICATION_CREDENTIALS=$PWD"/gcloud_key.json"
--- code/test_plot.py
+++ code/test_plot.py
@@ -1,8 +1,6 @@
 from utils import ScaraRobot
 
 if __name__ == "__main__":
-    robot = ScaraRobot(
-        config_dir='scara_config.json',
-    )
+    robot = ScaraRobot(machine_config_path='machine_config.json')
 
     robot.start()
